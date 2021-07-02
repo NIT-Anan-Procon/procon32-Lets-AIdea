7371483885f84193b76e849a89af248acd3f3aec
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-07-03 00:45:28 +0900

    :confetti_ball: 得点を表示

---
+++ js/02-03.js
@@ -0,0 +1,34 @@
+let user_point = [
+  [1, 1],
+  [19, 19],
+  [1, 4],
+  [45, 45],
+];
+
+for (let i = 0; i < user_point.length - 1; i++) {
+  for (let j = i; j < user_point.length - 1; j++) {
+    if (
+      user_point[i][0] + user_point[i][1] <
+      user_point[j + 1][0] + user_point[j + 1][1]
+    ) {
+      let tmp = user_point[i][0];
+      user_point[i][0] = user_point[j + 1][0];
+      user_point[j + 1][0] = tmp;
+      tmp = user_point[i][1];
+      user_point[i][1] = user_point[j + 1][1];
+      user_point[j + 1][1] = tmp;
+    }
+  }
+}
+for (let i = 0; i < user_point.length; i++)
+  console.log(user_point[i][0] + " " + user_point[i][1]);
+
+sum_pt1.innerHTML = user_point[0][0] + user_point[0][1];
+ex_pt1.innerHTML = user_point[0][0];
+ans_pt1.innerHTML = user_point[0][1];
+sum_pt2.innerHTML = user_point[1][0] + user_point[1][1];
+ex_pt2.innerHTML = user_point[1][0];
+ans_pt2.innerHTML = user_point[1][1];
+sum_pt3.innerHTML = user_point[2][0] + user_point[2][1];
+ex_pt3.innerHTML = user_point[2][0];
+ans_pt3.innerHTML = user_point[2][1];
