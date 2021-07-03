8799e4f8931719200eb284153cecc82fcc23ca87
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-07-03 13:49:48 +0900

    :cactus: 項目名を記入

--- js/02-03.js
+++ js/02-03.js
@@ -20,15 +20,13 @@ for (let i = 0; i < user_point.length - 1; i++) {
     }
   }
 }
-for (let i = 0; i < user_point.length; i++)
-  console.log(user_point[i][0] + " " + user_point[i][1]);
 
-sum_pt1.innerHTML = user_point[0][0] + user_point[0][1];
-ex_pt1.innerHTML = user_point[0][0];
-ans_pt1.innerHTML = user_point[0][1];
-sum_pt2.innerHTML = user_point[1][0] + user_point[1][1];
-ex_pt2.innerHTML = user_point[1][0];
-ans_pt2.innerHTML = user_point[1][1];
-sum_pt3.innerHTML = user_point[2][0] + user_point[2][1];
-ex_pt3.innerHTML = user_point[2][0];
-ans_pt3.innerHTML = user_point[2][1];
+sum_pt1.innerHTML += user_point[0][0] + user_point[0][1];
+ex_pt1.innerHTML += user_point[0][0];
+ans_pt1.innerHTML += user_point[0][1];
+sum_pt2.innerHTML += user_point[1][0] + user_point[1][1];
+ex_pt2.innerHTML += user_point[1][0];
+ans_pt2.innerHTML += user_point[1][1];
+sum_pt3.innerHTML += user_point[2][0] + user_point[2][1];
+ex_pt3.innerHTML += user_point[2][0];
+ans_pt3.innerHTML += user_point[2][1];
