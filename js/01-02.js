6dacb10c709714eb4706e5d473ac2189b4eecdd1
Togo555-mp4 <5t1ng20x@gmail.com>
2021-07-04 18:38:44 +0900

    :sparkles:アイコンに名前を表示

--- js/01-02.js
+++ js/01-02.js
@@ -1,7 +1,9 @@
-let play=4, tx='', cltext='#000000', clback='#f0f8ff', explane=["ライオンが座っている", "ライオンがたたずんでいる",　"ライオンがいる", "犬が草原にいる"];
+let tx='', cltext='#000000', clback='#f0f8ff';
+let explane=["ライオンが座っている", "ライオンがたたずんでいる", "ライオンがいる", "犬が草原にいる"];
+let people=["マコ", "ケイ", "カコ", "アキヒト" ];
 
-for(let i=0; i<play; i++){
-    tx=tx+'<img src="img/icon_frame.png">';
+for(let i=0; i<people.length; i++){
+    tx=tx+'<div id=icon><img src="img/icon_frame.png"><p>'+people[i]+'</p></div>';
     tx=tx+'<button type="button" id="color'+i+'" onclick="changeColor(\'color'+i+'\')">';
     tx=tx+explane[i];
     tx=tx+'</button>';
