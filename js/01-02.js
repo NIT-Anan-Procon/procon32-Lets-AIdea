0cf58c4320c4727f99314e60cf29eac76eeea50b
Togo555-mp4 <5t1ng20x@gmail.com>
2021-07-05 23:01:31 +0900

    :wheelchair: ボタンを押した際の色変化をわかりやすくした

--- js/01-02.js
+++ js/01-02.js
@@ -1,25 +1,29 @@
-let tx='', cltext='#000000', clback='#f0f8ff';
-let explane=["ライオンが座っている", "ライオンがたたずんでいる", "ライオンがいる", "犬が草原にいる"];
 let people=["マコ", "ケイ", "カコ", "アキヒト" ];
+let explane=["ライオンが座っている", "ライオンがたたずんでいる", "ライオンがいる", "犬が草原にいる"];
+let tx='', i;
+let cl=[];
 
-for(let i=0; i<people.length; i++){
+for(i=0; i<people.length; i++){
+    cl[i]='OFF';
+}
+
+for(i=0; i<people.length; i++){
     tx=tx+'<div id=icon><img src="img/icon_frame.png"><p>'+people[i]+'</p></div>';
-    tx=tx+'<button type="button" id="color'+i+'" onclick="changeColor(\'color'+i+'\')">';
+    tx=tx+'<button type="button" id="color'+i+'" onclick="changeColor(\'color'+i+'\', '+i+')">';
     tx=tx+explane[i];
     tx=tx+'</button>';
 }
 player.innerHTML=tx;
 
-function changeColor(idname){
+function changeColor(idname, n){
     let obj = document.getElementById(idname);
-    if(cltext==='#000000'){
-        cltext='#ffffff';
-        clback='#4169e1';
-
+    if(cl[n]==='OFF'){
+        obj.style.color='#ffffff';
+        obj.style.backgroundColor='#4169e1';
+        cl[n]='ON'
     }else{
-        cltext='#000000';
-        clback='#f0f8ff';
+        obj.style.color='';
+        obj.style.backgroundColor='';
+        cl[n]='OFF'
     }
-    obj.style.color=cltext;
-    obj.style.backgroundColor=clback;
 }
