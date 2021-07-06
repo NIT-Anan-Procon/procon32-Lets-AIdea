cc85ae317a29b10a316629517f2b1b99df70b94c
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-07-06 22:50:04 +0900

    :gift: 順位を装飾

--- js/02-03.js
+++ js/02-03.js
@@ -1,8 +1,8 @@
 let user_point = [
-  [1, 1],
-  [19, 19],
-  [1, 4],
-  [45, 45],
+  [1, 1, "千夜"],
+  [19, 19, "智乃"],
+  [1, 4, "理世"],
+  [45, 45, "心愛"],
 ];
 
 for (let i = 0; i < user_point.length - 1; i++) {
@@ -17,16 +17,22 @@ for (let i = 0; i < user_point.length - 1; i++) {
       tmp = user_point[i][1];
       user_point[i][1] = user_point[j + 1][1];
       user_point[j + 1][1] = tmp;
+      tmp = user_point[i][2];
+      user_point[i][2] = user_point[j + 1][2];
+      user_point[j + 1][2] = tmp;
     }
   }
 }
 
+icon1.innerHTML += "<p>" + user_point[0][2] + "</p>";
 sum_pt1.innerHTML += user_point[0][0] + user_point[0][1];
 ex_pt1.innerHTML += user_point[0][0];
 ans_pt1.innerHTML += user_point[0][1];
+icon2.innerHTML += "<p>" + user_point[1][2] + "</p>";
 sum_pt2.innerHTML += user_point[1][0] + user_point[1][1];
 ex_pt2.innerHTML += user_point[1][0];
 ans_pt2.innerHTML += user_point[1][1];
+icon3.innerHTML += "<p>" + user_point[2][2] + "</p>";
 sum_pt3.innerHTML += user_point[2][0] + user_point[2][1];
 ex_pt3.innerHTML += user_point[2][0];
 ans_pt3.innerHTML += user_point[2][1];
