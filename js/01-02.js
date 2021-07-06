9e6c2601fe9d2475f567567d51366a423fedb44c
Togo555-mp4 <5t1ng20x@gmail.com>
2021-07-06 18:09:22 +0900

    :hammer: ボタンの仕様変更と決定ボタンで次のページに行くようにした

--- js/01-02.js
+++ js/01-02.js
@@ -1,29 +1,49 @@
 let people=["マコ", "ケイ", "カコ", "アキヒト" ];
 let explane=["ライオンが座っている", "ライオンがたたずんでいる", "ライオンがいる", "犬が草原にいる"];
 let tx='', i;
-let cl=[];
+let sw=[], clt=[], clb=[];
 
 for(i=0; i<people.length; i++){
-    cl[i]='OFF';
+    sw[i]='OFF';
+    clt[i]='';
+    clb[i]='';
 }
 
 for(i=0; i<people.length; i++){
     tx=tx+'<div id=icon><img src="img/icon_frame.png"><p>'+people[i]+'</p></div>';
-    tx=tx+'<button type="button" id="color'+i+'" onclick="changeColor(\'color'+i+'\', '+i+')">';
+    tx=tx+'<button type="button" id="color'+i+'" onclick="changeColor('+i+')">';
     tx=tx+explane[i];
     tx=tx+'</button>';
 }
 player.innerHTML=tx;
 
-function changeColor(idname, n){
-    let obj = document.getElementById(idname);
-    if(cl[n]==='OFF'){
-        obj.style.color='#ffffff';
-        obj.style.backgroundColor='#4169e1';
-        cl[n]='ON'
+function chack(n){
+    if(sw[n]==='OFF'){
+        sw[n]='ON'
     }else{
-        obj.style.color='';
-        obj.style.backgroundColor='';
-        cl[n]='OFF'
+        sw[n]='OFF'
+    }
+    for(i=0; i<people.length; i++){
+        if(n!=i){
+            sw[i]='OFF';
+            clt[i]='';
+            clb[i]='';
+        }
+    }
+}
+
+function changeColor(n){
+    if(sw[n]==='OFF'){
+        clt[n]='#ffffff';
+        clb[n]='#4169e1';
+        chack(n);
+    }else{
+        clt[n]='';
+        clb[n]='';
+        chack(n);
+    }
+    for(i=0; i<people.length; i++){
+        eval("document.getElementById('color"+i+"').style.color=clt["+i+"];");
+        eval("document.getElementById('color"+i+"').style.backgroundColor=clb["+i+"];");
     }
 }
