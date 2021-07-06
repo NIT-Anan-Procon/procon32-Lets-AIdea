ca9a90c1fe08606e4ac4228c9011299c49d3ee07
Togo555-mp4 <5t1ng20x@gmail.com>
2021-07-06 18:31:19 +0900

    :speech_balloon:コメントアウトを追加

--- js/01-02.js
+++ js/01-02.js
@@ -1,14 +1,16 @@
-let people=["マコ", "ケイ", "カコ", "アキヒト" ];
-let explane=["ライオンが座っている", "ライオンがたたずんでいる", "ライオンがいる", "犬が草原にいる"];
-let tx='', i;
-let sw=[], clt=[], clb=[];
+let people=["トリコ", "ココ", "コマツ", "サニー" ]; //プレイヤーネーム
+let explane=["ライオンが座っている", "ライオンがたたずんでいる", "ライオンがいる", "犬が草原にいる"]; //説明文
+let tx='', i;　　　　　　　　 // tx:htmlのコード保存用  i:for文用  
+let sw=[], clt=[], clb=[];   // sw[]:ボタンの状態  clt[]:テキストの色  clb[]:ボタンの色
 
+//配列の初期化
 for(i=0; i<people.length; i++){
     sw[i]='OFF';
     clt[i]='';
     clb[i]='';
 }
 
+//htmlのコード書き込み
 for(i=0; i<people.length; i++){
     tx=tx+'<div id=icon><img src="img/icon_frame.png"><p>'+people[i]+'</p></div>';
     tx=tx+'<button type="button" id="color'+i+'" onclick="changeColor('+i+')">';
@@ -17,6 +19,11 @@ for(i=0; i<people.length; i++){
 }
 player.innerHTML=tx;
 
+/*
+ボタンの状態の処理を行う関数
+ボタンが押されているものをON
+ボタンが押されていないものをOFF、
+*/
 function chack(n){
     if(sw[n]==='OFF'){
         sw[n]='ON'
@@ -32,6 +39,11 @@ function chack(n){
     }
 }
 
+/*
+ボタンの色を変更する関数
+押されたボタンの色の変更
+押されていないボタンの色をリセット
+*/
 function changeColor(n){
     if(sw[n]==='OFF'){
         clt[n]='#ffffff';
