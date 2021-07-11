44f1212a8fbe2dea1752b861c876c796fce29220
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-07-11 23:49:24 +0900

    :gem: jsファイル合成

--- quiz/script.js
+++ quiz/script.js
@@ -1,10 +1,33 @@
+let ai_ex = "ライオンが座っている",
+  ng_word = ["ライオン", "座っている"];
+for (let i = 1; i < ng_word.length; i++)
+  ng_word[0] = ng_word[0] + " " + ng_word[i];
+ai.innerHTML =
+  '<span style="font-size: 5vh; line-height: 6vh; margin: 1vh; color: blue;">AI説明文</span><br>' +
+  ai_ex;
+ng.innerHTML =
+  '<span style="font-size: 5vh; line-height: 6vh; margin: 1vh; color: red;">NGワード</span><br>' +
+  ng_word[0];
+
+/* ------------------------------------------- */
+
+let hu_ex = "ライオンが座っている";
+ex.innerHTML = hu_ex;
+function checkAnswer() {
+  image1.innerHTML += '<div id="check"><img src="img/circle.png"></div>';
+  image2.innerHTML += '<div id="check"><img src="img/cross.png"></div>';
+  image3.innerHTML += '<div id="check"><img src="img/cross.png"></div>';
+  image4.innerHTML += '<div id="check"><img src="img/cross.png"></div>';
+}
+
+/* ------------------------------------------- */
+
 let user_point = [
   [1, 1, "千夜"],
   [19, 19, "智乃"],
   [1, 4, "理世"],
   [45, 45, "心愛"],
 ];
-
 for (let i = 0; i < user_point.length - 1; i++) {
   for (let j = i; j < user_point.length - 1; j++) {
     if (
@@ -23,7 +46,6 @@ for (let i = 0; i < user_point.length - 1; i++) {
     }
   }
 }
-
 icon1.innerHTML += "<p>" + user_point[0][2] + "</p>";
 sum_pt1.innerHTML += user_point[0][0] + user_point[0][1];
 ex_pt1.innerHTML += user_point[0][0];
