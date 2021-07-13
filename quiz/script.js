5bd8bce2c2c3ca0c4d1ce2b895a79fe74352dc99
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-07-13 17:46:02 +0900

    :boom: 4位の追加

--- quiz/script.js
+++ quiz/script.js
@@ -25,10 +25,10 @@ try {
     /* ------------------------------------------- */
 
     let user_point = [
-      [1, 1, "千夜"],
-      [19, 19, "智乃"],
-      [1, 4, "理世"],
-      [45, 45, "心愛"],
+      [8, 20, "心愛"],
+      [9, 13, "智乃"],
+      [5, 14, "理世"],
+      [2, 17, "千夜"],
     ];
     for (let i = 0; i < user_point.length - 1; i++) {
       for (let j = i; j < user_point.length - 1; j++) {
@@ -48,17 +48,25 @@ try {
         }
       }
     }
-    icon1.innerHTML += "<p>" + user_point[0][2] + "</p>";
-    sum_pt1.innerHTML += user_point[0][0] + user_point[0][1];
-    ex_pt1.innerHTML += user_point[0][0];
-    ans_pt1.innerHTML += user_point[0][1];
-    icon2.innerHTML += "<p>" + user_point[1][2] + "</p>";
-    sum_pt2.innerHTML += user_point[1][0] + user_point[1][1];
-    ex_pt2.innerHTML += user_point[1][0];
-    ans_pt2.innerHTML += user_point[1][1];
-    icon3.innerHTML += "<p>" + user_point[2][2] + "</p>";
-    sum_pt3.innerHTML += user_point[2][0] + user_point[2][1];
-    ex_pt3.innerHTML += user_point[2][0];
-    ans_pt3.innerHTML += user_point[2][1];
+    for (let i = 0; i < user_point.length; i++) {
+      eval(
+        "icon" +
+          (i + 1) +
+          ".innerHTML += '<p>' + user_point[" +
+          i +
+          "][2] + '</p>';"
+      );
+      eval(
+        "sum_pt" +
+          (i + 1) +
+          ".innerHTML += user_point[" +
+          i +
+          "][0] + user_point[" +
+          i +
+          "][1];"
+      );
+      eval("ex_pt" + (i + 1) + ".innerHTML += user_point[" + i + "][0];");
+      eval("ans_pt" + (i + 1) + ".innerHTML += user_point[" + i + "][1];");
+    }
   }
 }
