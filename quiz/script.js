14a24f8bb520556fbd6c0e128ddf876f1ebcc350
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-07-13 18:16:18 +0900

    :ok_woman: 同順位を表示するように修正

--- quiz/script.js
+++ quiz/script.js
@@ -10,25 +10,28 @@ try {
     '<span style="font-size: 5vh; line-height: 6vh; margin: 1vh; color: red;">NGワード</span><br>' +
     ng_word[0];
 } catch (e) {
-  /* ------------------------------------------- */
+  /* ----------------------------------------- */
 
   try {
     let another_ex = "ライオンが座っている";
     another.innerHTML = another_ex;
     function checkAnswer() {
-      image1.innerHTML += '<div id="check"><img src="img/circle.png"></div>';
+      image1.innerHTML += '<div id="check"><a><img src="img/circle.png"></div>';
       image2.innerHTML += '<div id="check"><img src="img/cross.png"></div>';
       image3.innerHTML += '<div id="check"><img src="img/cross.png"></div>';
       image4.innerHTML += '<div id="check"><img src="img/cross.png"></div>';
+      window.setTimeout(function () {
+        window.location.href = "result.html";
+      }, 3000);
     }
   } catch (e) {
-    /* ------------------------------------------- */
+    /* --------------------------------------- */
 
     let user_point = [
-      [8, 20, "心愛"],
       [9, 13, "智乃"],
-      [5, 14, "理世"],
+      [8, 20, "心愛"],
       [2, 17, "千夜"],
+      [5, 14, "理世"],
     ];
     for (let i = 0; i < user_point.length - 1; i++) {
       for (let j = i; j < user_point.length - 1; j++) {
@@ -47,6 +50,19 @@ try {
           user_point[j + 1][2] = tmp;
         }
       }
+    }
+    user_point[0].push(1);
+    for (let i = 1; i < user_point.length; i++) {
+      if (
+        user_point[i - 1][0] + user_point[i - 1][1] ==
+        user_point[i][0] + user_point[i][1]
+      )
+        user_point[i].push(user_point[i - 1][3]);
+      else user_point[i].push(i + 1);
+      if (user_point[i][3] == 1) user_point[i][3] += "st";
+      else if (user_point[i][3] == 2) user_point[i][3] += "nd";
+      else if (user_point[i][3] == 3) user_point[i][3] += "rd";
+      else if (user_point[i][3] == 4) user_point[i][3] += "th";
     }
     for (let i = 0; i < user_point.length; i++) {
       eval(
@@ -67,6 +83,9 @@ try {
       );
       eval("ex_pt" + (i + 1) + ".innerHTML += user_point[" + i + "][0];");
       eval("ans_pt" + (i + 1) + ".innerHTML += user_point[" + i + "][1];");
+    }
+    for (let i = 1; i < user_point.length; i++) {
+      eval("rank" + (i + 1) + ".innerHTML = user_point[" + i + "][3];");
     }
   }
 }
